c76b9c0ac86bb79c689739c2985fbc1892a830c5
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-08 17:41:43 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Creo due variabili per la distanza e l'età del cliente
+
+let customerTrip = parseInt( prompt ('How many chilomiters will your trip take?'));
+
+let customerAge = parseInt (prompt ('How old are you?'));
+
+console.log(customerTrip);
+console.log(customerAge);
+
+// Setto il costo del biglietto per km
+
+const ticketPrice = customerTrip * 0.276
+
+console.log(ticketPrice);
+
